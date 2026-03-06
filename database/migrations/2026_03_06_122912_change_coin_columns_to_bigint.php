da7c2e39ffaa7fbb4068f9c7e80054be89626591
ved <vedtomer@gmail.com>
2026-03-06 21:46:25 +0530

    fix

--- database/migrations/2026_03_06_122912_change_coin_columns_to_bigint.php
+++ database/migrations/2026_03_06_122912_change_coin_columns_to_bigint.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->bigInteger('coins')->change();
+            $table->bigInteger('coins')->default(0)->change();
         });
 
         Schema::table('coin_transactions', function (Blueprint $table) {
