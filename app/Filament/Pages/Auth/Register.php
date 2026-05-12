da3eb5a6f9ec1d071edaae64f46da9b321777c32
ved <vedtomer@gmail.com>
2026-05-13 01:50:31 +0530

    fix

--- app/Filament/Pages/Auth/Register.php
+++ app/Filament/Pages/Auth/Register.php
@@ -5,11 +5,27 @@ namespace App\Filament\Pages\Auth;
 use Filament\Pages\Auth\Register as BaseRegister;
 use Illuminate\Database\Eloquent\Model;
 
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Support\Facades\Hash;
+
 class Register extends BaseRegister
 {
     protected function handleRegistration(array $data): Model
     {
         $data['type'] = 'user';
         return parent::handleRegistration($data);
+    }
+
+    protected function getPasswordFormComponent(): Component
+    {
+        return TextInput::make('password')
+            ->label(__('filament-panels::pages/auth/register.form.password.label'))
+            ->password()
+            ->revealable(filament()->arePasswordsRevealable())
+            ->required()
+            ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+            ->same('passwordConfirmation')
+            ->validationAttribute(__('filament-panels::pages/auth/register.form.password.validation_attribute'));
     }
 }
