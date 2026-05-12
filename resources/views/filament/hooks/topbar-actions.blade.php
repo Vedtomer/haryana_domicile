71391765912550dc5e890cbaa3502f16b30e03da
ved <vedtomer@gmail.com>
2026-05-12 22:58:40 +0530

    FIX

--- resources/views/filament/hooks/topbar-actions.blade.php
+++ resources/views/filament/hooks/topbar-actions.blade.php
@@ -1,12 +1,7 @@
 @if(auth()->user()->type !== 'admin')
-<div class="flex items-center">
-    <div class="flex items-center gap-2 px-3 py-1 bg-amber-500 rounded-full cursor-default select-none">
-        <i class="fa-solid fa-coins text-white text-sm"></i>
-        <div class="flex flex-col leading-none">
-            <span class="text-sm font-bold text-white leading-none">
-                {{ number_format(auth()->user()->coins ?? 0) }}
-            </span>
-        </div>
-    </div>
+<div class="flex items-center pr-4">
+    <x-filament::badge color="warning" icon="heroicon-s-circle-stack" size="lg">
+        {{ number_format(auth()->user()->coins ?? 0) }} Coins
+    </x-filament::badge>
 </div>
 @endif
