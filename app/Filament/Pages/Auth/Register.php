3a9aeda79b8ad3f6963dc51e3fc1882a28ca3dfb
ved <vedtomer@gmail.com>
2026-05-13 13:42:03 +0530

    fix

--- app/Filament/Pages/Auth/Register.php
+++ app/Filament/Pages/Auth/Register.php
@@ -14,7 +14,20 @@ class Register extends BaseRegister
     protected function handleRegistration(array $data): Model
     {
         $data['type'] = 'user';
-        return parent::handleRegistration($data);
+        $data['coins'] = 1000;
+        
+        $user = parent::handleRegistration($data);
+        
+        \App\Models\CoinTransaction::create([
+            'user_id' => $user->id,
+            'amount' => 1000,
+            'balance_after' => 1000,
+            'type' => \App\Models\CoinTransaction::TYPE_ADMIN_CREDIT,
+            'description' => 'Welcome Bonus',
+            'created_by' => $user->id,
+        ]);
+        
+        return $user;
     }
 
     protected function getPasswordFormComponent(): Component
