5374b24678fad3e8db9f0d4571e1feb69d0cf432
ved <vedtomer@gmail.com>
2026-08-02 23:50:08 +0530

    build: update frontend assets and manifest for new build deployment

---
+++ public/build/assets/Edit-CWO7z6MB.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-D8TjCuog.js";import o from"./Create-BjsElyz7.js";/* empty css            */import"./AdminLayout-CHYraSck.js";function s(t){return r.jsx(o,{...t})}export{s as default};
