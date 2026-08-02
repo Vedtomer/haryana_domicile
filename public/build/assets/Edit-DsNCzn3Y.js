a1f3f2fa1c3e762d26f4d3553a703a321c31a491
ved <vedtomer@gmail.com>
2026-08-02 23:25:45 +0530

    feat: add user type display and pagination to user management index page

---
+++ public/build/assets/Edit-DsNCzn3Y.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-DDj_G_zM.js";import o from"./Create-CuljopFT.js";/* empty css            */import"./AdminLayout-DZ3ofMdm.js";function s(t){return r.jsx(o,{...t})}export{s as default};
