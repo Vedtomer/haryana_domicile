cab63260999a7bed0711ae7375fbccf878bbc68c
ved <vedtomer@gmail.com>
2026-08-03 00:00:36 +0530

    chore: update build assets and manifest following project recompilation

---
+++ public/build/assets/Edit-D7E1qiAR.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-xPO9rQB3.js";import o from"./Create-DwiM5W5Q.js";/* empty css            */import"./AdminLayout-Ce5TGD7z.js";function s(t){return r.jsx(o,{...t})}export{s as default};
