ea2e2f6ee9be5a6f376b35478f4c7fdb8b0f6b33
ved <vedtomer@gmail.com>
2026-08-02 23:30:35 +0530

    refactor: update build assets and manifest for project frontend updates

---
+++ public/build/assets/Edit-Cpc-EMJ1.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-DFsc62QD.js";import o from"./Create-EBwuOxLr.js";/* empty css            */import"./AdminLayout-BmbGi6PE.js";function s(t){return r.jsx(o,{...t})}export{s as default};
--- public/build/assets/Edit-LvZUWCUX.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CWmrFgZ8.js";import o from"./Create-DKLX_nFc.js";/* empty css            */import"./AdminLayout-DVjt5Zaz.js";function s(t){return r.jsx(o,{...t})}export{s as default};
