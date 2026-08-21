2b9c9ec204e4827ac986b825dc9a99dfb056c0f5
whitedevil <whitedevil@localhost>
2026-08-21 14:20:41 +0530

    Auto deployment update: 2026-08-21 14:20:40

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,7 @@ Route::middleware('auth')->group(function () {
         Route::resource('coin-requests', \App\Http\Controllers\Admin\CoinPurchaseRequestController::class)->only(['index', 'create', 'store', 'update']);
         Route::resource('users', \App\Http\Controllers\Admin\UserController::class);
         Route::patch('users/{user}/toggle-status', [\App\Http\Controllers\Admin\UserController::class, 'toggleStatus'])->name('users.toggle-status');
+        Route::post('users/{user}/add-coins', [\App\Http\Controllers\Admin\UserController::class, 'addCoins'])->name('users.add-coins');
         Route::get('profile', [\App\Http\Controllers\Admin\ProfileController::class, 'edit'])->name('profile.edit');
         Route::put('profile', [\App\Http\Controllers\Admin\ProfileController::class, 'update'])->name('profile.update');
 
